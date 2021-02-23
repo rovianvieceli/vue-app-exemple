6c084bcb8ebe82f3b05f80528ce8b6068f1a1c5b
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2021-02-23 11:38:42 -0300

    Vuejs exemplo de listagem

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" :to="'/'">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="'/contact'">Contact</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Posts from "./components/Posts";
+import Contact from "./components/Contact";
+
+export default {
+  name: "App",
+  components: {
+    Posts,
+    Contact
+  }
+};
+</script>
+
+<style>
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <table class="table table-striped mt-4">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">User ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Body</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <th scope="row">{{ post.id }}</th>
+          <td>{{ post.userId }}</td>
+          <td>{{ post.title }}</td>
+          <td>{{ post.body }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Posts",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  created() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(posts => {
+      this.posts = posts.data;
+    });
+  }
+};
+</script>
+
+<style>
+</style>
